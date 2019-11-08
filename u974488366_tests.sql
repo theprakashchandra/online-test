a66b8a9326660da5b357430828c628cb12e0813e
Prakash Chandra <39235666+theprakashchandra@users.noreply.github.com>
2019-11-08 22:46:46 +0530

    database : online_tests

--- u974488366_tests.sql
+++ u974488366_tests.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `u974488366_tests`
+-- Database: `online_tests`
 --
 
 -- --------------------------------------------------------
